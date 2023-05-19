dadfa2811de976c6cf986a03044445c312c6f164
Maram ZITOUNI <zitouni.maram20@gmail.com>
2023-05-19 20:48:12 +0200

    add products in product-list accessible by CLOTHING tab

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,13 +2,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+// Material section
+import {MaterialExampleModule} from '../../material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatNativeDateModule} from '@angular/material/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+
+    // Material
+    MatSlideToggleModule,
+    MaterialExampleModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [ProductListComponent]
 })
