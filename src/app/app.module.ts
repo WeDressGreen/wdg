b2784e83d4b0403e6f9c6a7b4b18b11f4a0e124d
Maram ZITOUNI <zitouni.maram20@gmail.com>
2023-05-29 21:51:08 +0200

    general style fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,19 +11,21 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
-
+import { AccountComponent } from './account/account.component';
 
 
 // Routing
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     BodyComponent,
+    AccountComponent,
   ],
   imports: [
     BrowserModule,
